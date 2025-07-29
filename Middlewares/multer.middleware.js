176ad5f770616974d6b73cffe055ce849e3d82a8
Karan Kumar <karan.kumar800433@gmail.com>
2025-07-29 10:45:28 +0530

    one more change

--- Middlewares/multer.middleware.js
+++ Middlewares/multer.middleware.js
@@ -1,14 +1,17 @@
+import path from 'path';
 import multer from 'multer';
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './public/temp')
+    const tempPath = process.env.NODE_ENV === 'production'
+      ? '/tmp' // Render's writable directory
+      : path.join(process.cwd(), 'public', 'temp');
+    cb(null, tempPath);
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix)
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix);
   }
-})
-
-const upload = multer({ storage: storage })
+});
+const upload = multer({ storage: storage });
 export default upload;
